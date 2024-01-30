f209a9108d53bb7767d1a2dc06d7fa92cf88d4cb
tastaw23 <younesmekki2@gmail.com>
2024-01-30 01:16:22 -0800

    bash

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,14 +1,3 @@
 #!/usr/bin/env ruby
-
-# Accept one argument
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /hbt+n/
-
-# Use the regular expression to find matches
-matches = input_string.scan(pattern)
-
-# Output the matches
-puts matches.join
+puts ARGV[0].scan(/hb{1}?tn/).join
 
